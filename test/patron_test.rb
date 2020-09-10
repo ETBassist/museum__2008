2c0045c41a75dbbf410466170c279f1df3dea85d
Eugene Theriault <hybridbassist@gmail.com>
2020-09-10 08:15:17 -0700

    Create test for add_interests method

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -14,4 +14,10 @@ class PatronTest < MiniTest::Test
     assert_equal 20, @patron_1.spending_money
     assert_equal [], @patron_1.interests
   end
+
+  def test_it_can_add_interests_to_patron
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
 end
